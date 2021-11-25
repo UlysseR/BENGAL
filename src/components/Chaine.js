651272babd151352b0b9767f355953f1900f6a2e
Lionel78570 <91317444+Lionel78570@users.noreply.github.com>
2021-11-25 12:32:44 +0100

    version_lio_pas_push

--- src/components/Chaine.js
+++ src/components/Chaine.js
@@ -2,9 +2,26 @@ import React from 'react';
 
 const Chaine = () => {
     return (
-        <div className='test'>
-            LA CHAINE
-        </div>
+        <>
+            <div className='test'>
+                LA CHAINE
+            </div>
+            <section>
+                <article>
+                    <h1>Tremplin, la chaine Youtube qui vous faut</h1>
+                    <p>
+                        Tremplin est une plateforme ou l’on peux retrouver toutes les ressouces, outils et état d’esprit à acquérir pour commencer dans l’entrepreneuriat.
+                        Une aide personnalisé pas à pas qui s’applique à toute sorte d’entrepreneuriat pour tout type de futurs entrepreneurs.
+                    </p>
+                    <button>
+                    LANCE TOI !
+                </button>
+                </article>
+                <div className='video'>
+                    <iframe></iframe>
+                </div>
+            </section>
+        </>
     );
 };
 
