8cad8579fceff1158c9c0b9512bed074b131af00
Yannis78 <yannisk789@gmail.com>
2021-11-25 23:14:51 +0100

    banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import ban from '../media/ban.png';
+import headerimg from '../media/header.png';
+import scrolldown from '../media/SVG/scroll-down.svg'
+import scrolldown2 from '../media/SVG/scroll-down2.svg'
 
 const Banner = () => {
     return (
-    <section className="banner">
-        <div className="ban-img">
-            <img src={ban}alt=""/>
-        </div>
-        <div className="inner-banner">
-            <p>TU VEUX ENTREPRENDRE ?</p>
-            <p>FONCE</p> 
-            <p>ON VA T’AIDER.</p>   
-        </div>
-    </section>
+        <>
+            <div className="container">
+                <img className="header-img" src={headerimg} alt="header-img"></img>
+                <div className="container-text">
+                    <p className="title-1">TU VEUX ENTREPRENDRE ?</p>
+                    <p className="title-2">FONCE,</p>
+                    <p className="title-3">ON VA T'AIDER.</p>
+                </div>
+            </div>
+            <div className="scroll-btn">  
+                <img className="scroll-down2" src={scrolldown2}></img>
+                <img className="scroll-down" src={scrolldown}></img>
+            </div>
+
+
+        </>
     );
 };
 
