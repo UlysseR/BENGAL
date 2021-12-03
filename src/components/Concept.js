9db5b3b78d5bf6e861599b302894a68fa01f58cd
Yannis78 <yannisk789@gmail.com>
2021-12-03 10:16:22 +0100

    new concept section

--- src/components/Concept.js
+++ src/components/Concept.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import more from '../media/SVG/more.svg';
-import lamp from '../media/illustration/lamp.svg';
+import React from "react";
+import more from "../media/SVG/more.svg";
+import lamp from "../media/illustration/lamp.svg";
 import { useEffect } from "react";
-import Aos from 'aos';
+import Aos from "aos";
 import "aos/dist/aos.css";
 const Concept = () => {
-    useEffect(() => {
-        Aos.init({ duration: 2000 });
-    }, []);
-    return (
-        <>
-        <div ></div>
-            <div className="container-concept">
-                <div  data-aos="fade-right">
-                <div id="concept" className="circle"></div>
-                <div  className="concept-title">Notre concept</div>
-
-                </div>
-                <img data-aos="fade-left"  className="lamp" src={lamp}></img>
-            </div>
-        </>
-    );
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+  return (
+    <>
+      <div></div>
+      <div className="container-concept">
+        <div data-aos="fade-right">
+          <div id="concept" className="circle"></div>
+          <div className="concept-title">Notre concept</div>
+        </div>
+        <div className="concept-content">
+         <div data-aos="fade" data-aos-delay="50" className="title-concept">Faciliter l’accessibilité à l’entrepreneuriat :</div> 
+          <div data-aos="fade" data-aos-delay="50" className="p-concept"> 
+          &#8226; Mettre à disposition des ressources et guides pour se lancer <br></br>
+          &#8226; Créer une communauté <br></br>
+          &#8226; d’entrepreneurs Motiver & inspirer <br></br>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Concept;
